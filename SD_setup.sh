b9b04740af3df39aa227fd7cb21fe72f0fde21ff
csmkzava <csmkzava@gmail.com>
2024-08-08 14:27:35 +0800

    change webui.sh to launch.py

--- SD_setup.sh
+++ SD_setup.sh
@@ -1,3 +1,4 @@
+########################################## Basic settings
 # Update software
 sudo apt update && sudo apt -y upgrade
 
@@ -12,36 +13,35 @@ echo -e '\nexport COMMANDLINE_ARGS="--listen --port 8889"' >> stable-diffusion-w
 # For install models in SD
 sudo apt -y install aria2
 
-#####################
-# Models
-## 2-dimentions
+
+########################################## Models, Lora, Embeddings...
+# Models: 2-dimentions
 #aria2c -c -x 16 -s 16 "https://civitai.com/api/download/models/5581" -o "stable-diffusion-webui/models/Stable-diffusion/AnythingElse_V4.safetensors"
 aria2c -c -x 16 -s 16 "https://civitai.com/api/download/models/57618" -o "stable-diffusion-webui/models/Stable-diffusion/Counterfeit-V3.safetensors"
 #aria2c -c -x 16 -s 16 "https://civitai.com/api/download/models/4224" -o "stable-diffusion-webui/models/Stable-diffusion/Dreamlike_Photoreal_2.safetensors"
 
-## Realistic
+# Models: Realistic
 #aria2c -c -x 16 -s 16 "https://civitai.com/api/download/models/501240" -o "stable-diffusion-webui/models/Stable-diffusion/Realistic_vision.safetensors"
 aria2c -c -x 16 -s 16 "https://civitai.com/api/download/models/507319" -o "stable-diffusion-webui/models/Stable-diffusion/LOFI_V5.safetensors"
 
-## 2.5D
+# Models: 2.5D
 #aria2c -c -x 16 -s 16 "https://civitai.com/api/download/models/64094" -o "stable-diffusion-webui/models/Stable-diffusion/never_ending_dream.safetensors"
 aria2c -c -x 16 -s 16 "https://civitai.com/api/download/models/4007" -o "stable-diffusion-webui/models/Stable-diffusion/ProtogenV2_2.safetensors"
 
-#####################
 # Lora
 aria2c -c -x 16 -s 16 "https://civitai.com/api/download/models/9208" -o "stable-diffusion-webui/models/Lora/EasyNegative.safetensors"
 aria2c -c -x 16 -s 16 "https://civitai.com/api/download/models/67325" -o "stable-diffusion-webui/models/Lora/Asian_girls_face.safetensors"
 
-#####################
 # Embeddings
 aria2c -c -x 16 -s 16 "https://civitai.com/api/download/models/20068" -o "stable-diffusion-webui/embeddings/badhandv4.safetensors"
 
-#####################
-# Extensions
+
+########################################## Extensions
 
 
 
 
 
-# Start up
-sudo bash stable-diffusion-webui/webui.sh
+########################################## Start-up
+#stable-diffusion-webui/webui.sh
+python3 stable-diffusion-webui/launch.py
