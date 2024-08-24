b5def2a81ed47f54b6559e7d9308a2d3814306b3
csmkzava <csmkzava@gmail.com>
2024-08-24 15:15:35 +0800

    add extensions 1.chinese 2.image-browser 3.tagger

--- SD_setup.sh
+++ SD_setup.sh
@@ -14,33 +14,56 @@ echo -e '\nexport COMMANDLINE_ARGS="--listen --port 8889"' >> stable-diffusion-w
 sudo apt -y install aria2
 
 
-########################################## Models, Lora, Embeddings...
-# Models: 2-dimentions
+########################################## Models
+# 2-dimentions
 #aria2c -c -x 16 -s 16 "https://civitai.com/api/download/models/5581" -o "stable-diffusion-webui/models/Stable-diffusion/AnythingElse_V4.safetensors"
 aria2c -c -x 16 -s 16 "https://civitai.com/api/download/models/57618" -o "stable-diffusion-webui/models/Stable-diffusion/Counterfeit-V3.safetensors"
 #aria2c -c -x 16 -s 16 "https://civitai.com/api/download/models/4224" -o "stable-diffusion-webui/models/Stable-diffusion/Dreamlike_Photoreal_2.safetensors"
 
-# Models: Realistic
+# Realistic
 #aria2c -c -x 16 -s 16 "https://civitai.com/api/download/models/501240" -o "stable-diffusion-webui/models/Stable-diffusion/Realistic_vision.safetensors"
 aria2c -c -x 16 -s 16 "https://civitai.com/api/download/models/507319" -o "stable-diffusion-webui/models/Stable-diffusion/LOFI_V5.safetensors"
 
-# Models: 2.5D
+# 2.5D
 #aria2c -c -x 16 -s 16 "https://civitai.com/api/download/models/64094" -o "stable-diffusion-webui/models/Stable-diffusion/never_ending_dream.safetensors"
 aria2c -c -x 16 -s 16 "https://civitai.com/api/download/models/4007" -o "stable-diffusion-webui/models/Stable-diffusion/ProtogenV2_2.safetensors"
 
-# Lora
+
+########################################## Lora
 aria2c -c -x 16 -s 16 "https://civitai.com/api/download/models/9208" -o "stable-diffusion-webui/models/Lora/EasyNegative.safetensors"
 aria2c -c -x 16 -s 16 "https://civitai.com/api/download/models/67325" -o "stable-diffusion-webui/models/Lora/Asian_girls_face.safetensors"
 
-# Embeddings
+
+########################################## Embeddings
 aria2c -c -x 16 -s 16 "https://civitai.com/api/download/models/20068" -o "stable-diffusion-webui/embeddings/badhandv4.safetensors"
 
 
 ########################################## Extensions
+# Chinese language
+git clone https://github.com/benlisquare/stable-diffusion-webui-localization-zh_TW.git 
+mv stable-diffusion-webui-localization-zh_TW stable-diffusion-webui/extensions
+
+## Install requirement
+sudo apt -y install moreutils
+
+## Use jq to update the JSON file and save the result to a new file
+jq --arg key "localization" --arg value "zh_TW" '.[$key] = $value' stable-diffusion-webui/config.json > stable-diffusion-webui/config.json.tmp && mv stable-diffusion-webui/config.json.tmp stable-diffusion-webui/config.json
+
+#-------------------------------------
+
+# Image-browser
+git clone https://github.com/AlUlkesh/stable-diffusion-webui-images-browser.git
+mv stable-diffusion-webui-images-browser stable-diffusion-webui/extensions
+
+# Tagger
+git clone https://github.com/toriato/stable-diffusion-webui-wd14-tagger.git
+mv stable-diffusion-webui-wd14-tagger stable-diffusion-webui/extensions
 
 
 
 
 
-########################################## Start-up
+
+
+########################################## Start SD
 stable-diffusion-webui/webui.sh
